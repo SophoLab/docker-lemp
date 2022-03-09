bbe83ba87732622af99ec372fc2a90c41162f098
Samuel Bouye <bouyesamuel@gmail.com>
2022-03-09 18:27:07 +0000

    add mysql service

--- website/index.php
+++ website/index.php
@@ -1,18 +1,39 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Hello there</title>
-        <style>
-            .center {
-                display: block;
-                margin-left: auto;
-                margin-right: auto;
-                width: 50%;
-            }
-        </style>
-    </head>
-    <body>
-        <img src="https://tech.osteel.me/images/2020/03/04/hello.gif" alt="Hello there" class="center">
-    </body>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Hello there</title>
+    <style>
+    .center {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        width: 50%;
+    }
+    </style>
+</head>
+
+<body>
+    <img src="https://tech.osteel.me/images/2020/03/04/hello.gif" alt="Hello there" class="center">
+
+    <?php
+    $pdo = new PDO("mysql:host=mysql;dbname=demo;charset=utf8", 'root', 'root');
+    $query = $pdo->query("select table_name from information_schema.tables where table_schema = 'demo'");
+    $tables = $query->fetchAll(PDO::FETCH_COLUMN);
+
+    if (empty($tables)) {
+        echo '<p class="center">There are no tables in this database <code>demo</code></p>';
+    } else {
+        echo '<p class="center">Database <code>demo</code> contains the following tables: </p>';
+        echo '<ul class="center">';
+        foreach ($tables as $tables) {
+            echo "<li>{$table}</li>";
+        }
+        echo '<ul>';
+    }
+
+    ?>
+</body>
+
 </html>
